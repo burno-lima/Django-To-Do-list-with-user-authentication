2a420a74b1f44f742e6bf3a9b12d65ef79b04a96
Bruno Luis Lima <bruno.lima@nappsolutions.com>
2022-08-29 14:05:54 -0300

    Create Task

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskList, TaskDeatil
+from .views import TaskList, TaskDeatil, TaskCreate
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),   
     path('task/<int:pk>/', TaskDeatil.as_view(), name='task'),   
+    path('task-create/', TaskCreate.as_view(), name='task-create'),   
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Task
 
 
@@ -13,3 +15,9 @@ class TaskDeatil(DetailView):
     model = Task
     context_object_name = 'task'
     template_name = 'core/task.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'    
+    success_url = reverse_lazy('tasks')
