68f9d4fa956ad228267a572b427508581d7b8d7a
Bruno Luis Lima <bruno.lima@nappsolutions.com>
2022-08-29 11:43:59 -0300

    Task List Page

--- core/views.py
+++ core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
 
-def taskList(request):
-    return HttpResponse('To Do list')
-
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
