b12faa102311bd17daa63fa10fcc95918af157ff
Bruno Gustavo <bruno.lima@nappsolutions.com>
2022-10-11 16:52:21 -0300

    Ajuste settings2

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
