d5666cae230c058c1cd555c1ba04cb2cfe819cbd
Bruno Gustavo <bruno.lima@nappsolutions.com>
2022-10-11 16:36:18 -0300

    Pronto para deploy

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +25,7 @@ SECRET_KEY = '&nc)r#c75rbw0k9_#=rfl+4jh#!kvg0+in6#i5sm7)@bslmy8r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-todoweb.herokuapp.com/']
 
 
 # Application definition
