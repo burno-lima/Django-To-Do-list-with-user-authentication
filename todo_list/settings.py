45d8ca032a76cb1ecbf62e21ac3a5c8990ad4c3b
Bruno Gustavo <bruno.lima@nappsolutions.com>
2022-10-11 16:58:57 -0300

    Ajuste url_database

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -120,3 +120,7 @@ LOGIN_URL = 'login'
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
